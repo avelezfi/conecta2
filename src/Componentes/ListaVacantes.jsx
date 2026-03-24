e4eacba078ad6a1df77d0d43afeae16f0748dcd7
chojuan69 <juan543silvadc@gmail.com>
2026-03-24 18:07:21 -0500

    Add Vacantes component to display job listings

--- src/Componentes/ListaVacantes.jsx
+++ src/Componentes/ListaVacantes.jsx
@@ -1,1 +1,11 @@
+import ListaVacantes from "./ListaVacantes";
+import { vacantes } from "../data/vacantes";
 
+export default function Vacantes() {
+  return (
+  <div>
+      <h2>Vacantes</h2>
+      <ListaVacantes vacantes={vacantes} />
+  </div>
+  )
+}
