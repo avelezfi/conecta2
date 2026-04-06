8d133deee3ed4e1478d7ad253d2ffd1003956e29
chojuan69 <juan543silvadc@gmail.com>
2026-04-05 21:55:44 -0500

    Add Buscar component to Vacantes page

--- src/pages/Vacantes.jsx
+++ src/pages/Vacantes.jsx
@@ -1,11 +1,13 @@
 
 import { vacantes } from "../data/vacantes";
 import Postulacion from "../Componentes/Postulacion";
+import Buscar from "../Componentes/Buscar";
 
 export default function Vacantes() {
   return (
   <div style = {container}>
       <h2>Vacantes</h2>
+    <Buscar vacantes={vacantes} />
     {vacantes.map((v) => (
      <Postulacion key={v.id} vacante={v} />
       ))}
