10838c78ddd8af87b197bec8524da1c6c157ee30
chojuan69 <juan543silvadc@gmail.com>
2026-04-05 21:02:05 -0500

    Replace ListaVacantes with Postulacion component

--- src/pages/Vacantes.jsx
+++ src/pages/Vacantes.jsx
@@ -1,11 +1,17 @@
-import ListaVacantes from "../Componentes/ListaVacantes";
+
 import { vacantes } from "../data/vacantes";
+import Postulacion from "../Componentes/Postulacion";
 
 export default function Vacantes() {
   return (
-  <div>
+  <div style = {container}>
       <h2>Vacantes</h2>
-      <ListaVacantes vacantes={vacantes} />
+    {vacantes.map((v) => (
+     <Postulacion key={v.id} vacante={v} />
+      ))}
   </div>
   );
 }
+
+
+const container = { border: '1px solid #ccc', padding: '15px', borderRadius: '8px', margin: '10px' };
